369871f70843ad99f5446f147866c22c53fec658
Singh <singhanga@ad.msoe.edu>
2020-02-12 10:46:22 -0600

    added some empty functions

--- javascript.js
+++ javascript.js
@@ -1,5 +1,19 @@
 class Website{
     constructor() {
+        let getDatabase = function () {
+            //just array of user objects with a string username and password
+        }
 
+        let verifyUsername = function () {
+
+        }
+
+        let verifyPassword = function () {
+
+        }
+
+        this.init = function () {
+            //initialise
+        }
     }
 }
