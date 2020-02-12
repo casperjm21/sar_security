05b4a97186f67d327d68a3c52aae936249d6729f
Singh <singhanga@ad.msoe.edu>
2020-02-12 11:14:15 -0600

    filled in some functions

--- javascript.js
+++ javascript.js
@@ -1,5 +1,7 @@
 class Website{
     constructor() {
+        let inputEmail;
+        let inputPassword;
         let getDatabase = function () {
             //just array of user objects with a string username and password
             let user = [
@@ -24,14 +26,25 @@ class Website{
                     password: "password"
                 }
                 ]
+            return user;
         }
 
         let verifyUsername = function () {
-
+            let user = getDatabase();
+            for(let i=0; i < user.length; i++){
+                if(user[i].email == inputEmail){
+                    return true;
+                }
+            }
         }
 
         let verifyPassword = function () {
-
+            let user = getDatabase();
+            for(let i=0; i < user.length; i++){
+                if(user[i].password == inputPassword){
+                    return true;
+                }
+            }
         }
 
         this.init = function () {
