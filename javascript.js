47c0e7b2c0e6eca4be5d9f29e604164b9ae057fb
Singh <singhanga@ad.msoe.edu>
2020-02-12 11:00:35 -0600

    added an array of emails and passwords

--- javascript.js
+++ javascript.js
@@ -2,7 +2,28 @@ class Website{
     constructor() {
         let getDatabase = function () {
             //just array of user objects with a string username and password
-            let user = [{}]
+            let user = [
+                {
+                    email: "singhanga@msoe.edu",
+                    password: "password"
+                },
+                {
+                    email: "angadriar@gmail.com",
+                    password: "password"
+                },
+                {
+                    email: "krenzva@msoe.edu",
+                    password: "password"
+                },
+                {
+                    email: "casperjm@msoe.edu",
+                    password: "password"
+                },
+                {
+                    email: "kaura@msoe.edu",
+                    password: "password"
+                }
+                ]
         }
 
         let verifyUsername = function () {
