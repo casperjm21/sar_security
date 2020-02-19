ad2e4cb16949c3fdb6d37233fbf748116b20f60c
Vincent <krenzva@msoe.edu>
2020-02-18 21:39:58 -0600

    Working on tests

---
+++ javascript.test.js
@@ -0,0 +1,18 @@
+const mockDatabase = jest.fn( {
+    email: "singhanga@msoe.edu",
+    password: "password",
+    isValid: true
+});
+
+
+verifyUsernamePassword('verify correct password username combo works', () => {
+
+}
+
+//Test a good email
+
+//Test a bad email
+
+//check email is sent if attempts are bad
+
+
